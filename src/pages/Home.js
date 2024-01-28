79ded0d8df08638df9e8f7ba0e2cae8b76ced6fa
Marcus Moore <marcus4moore@gmail.com>
2024-01-28 08:19:47 -0500

    Fetch Changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 // Home.js
-import React from 'react';
+import React, { useState } from 'react';
 import HomepageImage from '../components/HomepageImage';
 import { Typography, Grid, Button, TextField} from '@mui/material';
 import DownloadIcon from '@mui/icons-material/Download';
@@ -7,9 +7,31 @@ import DownloadIcon from '@mui/icons-material/Download';
 
 
 const Home = () => {
+  const [textFieldValue, setTextFieldValue] = useState('');
+
+  const handleButtonClick = async () => {
+    try {
+      // Make a fetch request
+      const response = await fetch(`http://172.206.255.161:8080/hash/${textFieldValue}`);
+      
+      // Check if the request was successful
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      // Parse the response JSON
+      const data = await response.json();
+
+      // Print the results in the console
+      console.log('Fetched data:', data);
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+    }
+  };
+
     return (
       <>
-          <Typography><DownloadIcon></DownloadIcon></Typography>
+          <Typography></Typography>
           
 
           <Grid container spacing={2} align="center" justify="center" alignItems="center">
@@ -26,11 +48,12 @@ const Home = () => {
                   label="Input Hash Here"
                   multiline
                   rows={8}
-                  defaultValue=""
+                  value={textFieldValue}
+                  onChange={(e) => setTextFieldValue(e.target.value)}
                 />
               </Grid>
               <Grid item xs={12} sm={12} md={2}>
-                <Button variant="outlined">De-Hash</Button>
+                <Button variant="outlined" onClick={handleButtonClick}>De-Hash</Button>
               </Grid>
               <Grid item xs={12} sm={12} md={2}>
                 <Button variant="outlined">Download<DownloadIcon></DownloadIcon></Button>
